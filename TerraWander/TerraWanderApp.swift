1c76a22b2a25b8d3dea7187594210be850e1e66b
far1h <farihmuhammad45@gmail.com>
2023-08-12 10:26:31 +0700

    Initial Commit

---
+++ TerraWander/TerraWanderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TerraWanderApp.swift
+//  TerraWander
+//
+//  Created by Farih Muhammad on 12/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TerraWanderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
