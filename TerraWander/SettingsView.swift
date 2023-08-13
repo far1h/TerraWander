fe6e8f8ddc8c9812f96f2b7bb1d91397e8747289
far1h <farihmuhammad45@gmail.com>
2023-08-13 11:25:47 +0700

    Try
    -settings page

---
+++ TerraWander/SettingsView.swift
@@ -0,0 +1,36 @@
+//
+//  SettingsView.swift
+//  TerraWander
+//
+//  Created by Farih Muhammad on 13/08/2023.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        GeometryReader{ geo in
+            ZStack{
+                Image("setting")
+                    .resizable()
+                    .scaledToFill()
+                    .edgesIgnoringSafeArea(.all)
+                    .frame(width: geo.size.width, height: geo.size.height, alignment: .center)
+                    .opacity(1.0)
+                Text("Settings")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                Spacer()
+                
+            }
+            
+        }
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
