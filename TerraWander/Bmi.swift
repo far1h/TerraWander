7f9d4c49a737dc556e7f0effe6bb2e2547a3f587
vlahovic09 <142207112+vlahovic09@users.noreply.github.com>
2023-08-16 16:34:35 +0700

    home screen baru
    bebas

---
+++ TerraWander/Bmi.swift
@@ -0,0 +1,15 @@
+//
+//  Bmi.swift
+//  TerraWander
+//
+//  Created by Jason Kosasih Saputra on 15/08/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct Bmi {
+    var value: Double
+    var date: Date
+    var color: Color
+}
